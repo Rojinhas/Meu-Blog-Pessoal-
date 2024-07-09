584d868a301dabd2156fcbc94a9ff38895afa579
rojinhas <ruthrojasdasilva12@gmail.com>
2024-07-09 11:39:47 -0300

    Meu Blog pessoal

---
+++ src/main/java/com/generation/meublogpessoal/MeublogpessoalApplication.java
@@ -0,0 +1,13 @@
+package com.generation.meublogpessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeublogpessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeublogpessoalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/meublogpessoal/MeublogpessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.meublogpessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeublogpessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
