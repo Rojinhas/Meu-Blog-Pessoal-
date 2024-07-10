3611cdce004e4888b5503879ff8677dbcad78be4
rojinhas <ruthrojasdasilva12@gmail.com>
2024-07-10 14:34:23 -0300

    Atividade concluída

---
+++ src/main/java/com/generation/meublogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,15 @@
+package com.generation.meublogpessoal.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.meublogpessoal.model.Postagem;
+
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+public Optional<Postagem> findByTitulo(String titulo);
+public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+
+}
